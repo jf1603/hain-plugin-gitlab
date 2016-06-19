61c768ab4b916fa9f1097ef9bb973932f327c864
unknown <jan@jfr-prodigy.tetsuo.lan>
2016-06-19 17:49:33 +0200

    Fixes #1 by rewriting some things based on talarari's suggestions. - Plugin is now able to handle reload of options - Redirect to preferences on error - Empty description is not shown as _NULL_ anymore - uses promises - new configuration option for _order_by_ - other minor optimizations

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = (pluginContext) => {
     const app = pluginContext.app;
     const shell = pluginContext.shell;
     const logger = pluginContext.logger;
-    const preferences = pluginContext.preferences.get();
+    const preferences = pluginContext.preferences;
 
     const TIMEOUT = 200;
     const FAKE = "FAKE_ID";
@@ -14,14 +14,29 @@ module.exports = (pluginContext) => {
     var client;
     var tokenSuffix;
 
+    const initClient = (prefs) => {
+        if (prefs.gitlabInstallation && prefs.privateToken) {
+            client = gitlab.createPromise({
+                api: prefs.gitlabInstallation + '/api/v3',
+                privateToken: prefs.privateToken
+            });
+            tokenSuffix = `?private_token=${prefs.privateToken}`;
+        }
+    };
+
+    function message(pTitle, pDesc, pIcon, pRedirect) {
+        return {
+            id: FAKE,
+            title: pTitle,
+            desc: pDesc,
+            icon: pIcon,
+            redirect: pRedirect
+        };
+    }
 
     function startup() {
-        client = gitlab.create({
-            api: `${preferences.gitlabInstallation}/api/v3`,
-            privateToken: preferences.privateToken
-        });
-
-        tokenSuffix = `?private_token=${preferences.privateToken}`;
+        initClient(preferences.get());
+        preferences.on('update', initClient);
     }
 
     function debounce(pFunction) {
@@ -35,64 +50,51 @@ module.exports = (pluginContext) => {
         }, TIMEOUT);
     }
 
-
-
     function queryProjects(pSearchString, pRes) {
-        pRes.add({
-            id: FAKE,
-            title: 'fetching...',
-            desc: `Projects from ${preferences.gitlabInstallation}`,
-            icon: '#fa fa-spinner fa-spin'
-        });
+        const prefs = preferences.get();
+        pRes.add(message('fetching...', `Projects from ${prefs.gitlabInstallation}`, '#fa fa-spinner fa-spin'));
         client.projects.search({
             query: pSearchString,
-            order_by: 'last_activity_at'
-        }, function(err, projects) {
+            order_by: prefs.orderBy
+        }).then((projects) => {
             pRes.remove(FAKE);
-            if (projects) {
-                for (var i = 0; i < projects.length; i++) {
-                    const project = projects[i];
-                    var projectResponse = {
-                        id: project.id,
-                        title: project.name_with_namespace,
-                        desc: project.description,
-                        payload: project.web_url,
-                        icon: project.avatar_url ? `${project.avatar_url}${tokenSuffix}` : '#fa fa-question'
-                    };
-                    pRes.add(projectResponse);
-                }
-            } else {
-                pRes.add({
-                    id: FAKE,
-                    payload: undefined,
-                    title: `No project found matching your criteria: ${pSearchString}. Be aware that search is case sensitive.`,
-                    desc: 'Try again with another search.'
-                });
+            if (projects.length == 0) {
+                pRes.remove(FAKE);
+                pRes.add(message(`No project found matching your criteria: '${pSearchString}'. Be aware that search is case sensitive.`, 'Try again with another search.', '#fa fa-question'));
+                return;
             }
+
+            projects.forEach(project => {
+                pRes.add({
+                    id: project.id,
+                    title: project.name_with_namespace,
+                    desc: project.description || '',
+                    payload: project.web_url,
+                    icon: project.avatar_url ? `${project.avatar_url}${tokenSuffix}` : '#fa fa-question'
+                });
+            });
+
+        }).catch(err => {
+            pRes.remove(FAKE);
+            pRes.add(message('Unable to fetch your results.', 'Make sure plugin preferences are correct, Click here to check', '#fa fa-exclamation', '/preferences'));
         });
     }
 
     function search(pSearchString, res) {
-        if (!preferences.gitlabInstallation || !preferences.privateToken) {
-            res.add({
-                id: FAKE,
-                title: 'Incomplete configuration...',
-                desc: 'Hit tab to redirect to /preferences for plugin configuration options. See hain-plugin-gitlab for details',
-                icon: '#fa fa-cogs',
-                redirect: '/preferences'
-            });
+        const prefs = preferences.get();
+
+        if (!client) {
+            res.add(message('Incomplete configuration...', 'Hit tab to redirect to /preferences for plugin configuration options. See hain-plugin-gitlab for details', '#fa fa-cogs', '/preferences'));
+            return;
         } else {
-            const searchString = pSearchString.trim();
-            if (searchString.length == 0) {
-                res.add({
-                    id: FAKE,
-                    title: 'Type in your query...',
-                    desc: `...to search ${preferences.gitlabInstallation} for projects.`,
-                    icon: '#fa fa-pencil'
-                });
-            } else {
-                debounce(queryProjects(searchString, res));
-            }
+            res.remove(FAKE);
+        }
+
+        const searchString = pSearchString.trim();
+        if (searchString.length == 0) {
+            res.add(message('Type in your query...', `...to search ${prefs.gitlabInstallation} for projects.`, '#fa fa-pencil'));
+        } else {
+            debounce(queryProjects(searchString, res));
         }
     }
 
